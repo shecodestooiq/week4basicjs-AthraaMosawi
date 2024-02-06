7709c0294b7f8c7d663ee310c903bb2ea2ed3f13
AthraaMosawi <athraamosawi169@gmail.com>
2024-02-07 01:10:09 +0300

    solve all the tasks

--- tasks.js
+++ tasks.js
@@ -1,31 +1,57 @@
 // Task 1
-function checkVotingEligibility() {
-
+function checkVotingEligibility(age) {
+  if (age >= 18) {
+    return 'You are eligible for voting';
+  } else {
+    return 'You are not eligible for voting';
+  }
 }
 
 // Task 2
 function printNumbersWithWhile() {
-
+  let i = 1;
+  while (i <= 5) {
+    console.log(i);
+    i++;
+  }
 }
 
 // Task 3
 function printEvenNumbersWithFor() {
-
+  for (let i = 2; i <= 10; i += 2) {
+    console.log(i);
+  }
 }
 
 // Task 4
-function getDayOfWeek() {
-
+function getDayOfWeek(num) {
+  switch (num) {
+    case 1:
+      return 'Sunday';
+    case 2:
+      return 'Monday';
+    case 3:
+      return 'Tuesday';
+    case 4:
+      return 'Wednesday';
+    case 5:
+      return 'Thursday';
+    case 6:
+      return 'Friday';
+    case 7:
+      return 'Saturday';
+    default:
+      return 'Invalid day';
+  }
 }
 
 // Task 5
-function sum() {
-
+function sum(num1, num2) {
+  return num1 + num2;
 }
 
 // Task 6
-//Uncomment and solve
-// const fruits = ;
+const fruits = ['apple', 'banana', 'orange', 'grape'];
 
 module.exports = {
   checkVotingEligibility,
